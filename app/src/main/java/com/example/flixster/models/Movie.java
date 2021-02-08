29553afd00f34dcabf50ad31229a50691afb2e10
NischithaCNagaraju <nischithacn@gmail.com>
2021-02-07 19:26:39 -0800

    "adding extra features Navigation flows and Passing data"

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,20 +3,28 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Movie {
-
+    int movieId;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+  public Movie(){}
 
   public Movie(JSONObject jsonObject) throws JSONException {
+      backdropPath = jsonObject.getString("backdrop_path");
       posterPath= jsonObject.getString("poster_path");
       title = jsonObject.getString("title");
       overview = jsonObject.getString("overview");
+      rating =jsonObject.getDouble("vote_average");
+      movieId = jsonObject.getInt("id");
   }
 
   public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -30,6 +38,9 @@ public class Movie {
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
+    public String getBackdropPathPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
 
     public String getTitle() {
         return title;
@@ -38,4 +49,11 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public double getRating(){ return rating;}
+
+    public int getMovieId(){
+      return movieId;
+  }
+
 }
